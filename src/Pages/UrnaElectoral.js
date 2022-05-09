e9d655e395de077f48b737c2629e5c2523103338
NicoleBtte <nicole.bustamante.trujillo@gmail.com>
2022-05-08 22:15:33 -0400

    Recuperacion de base de datos de urna

--- src/Pages/UrnaElectoral.js
+++ src/Pages/UrnaElectoral.js
@@ -1,8 +1,33 @@
+import { async } from '@firebase/util';
+import { doc } from 'firebase/firestore';
 import React from 'react'
+import {getUrna} from '../confs/firebaseConf'
+
+
+window.addEventListener('DOMContentLoaded', async() => {
+    
+    const querySnapshot = await getUrna()
+
+    let shtml = ''
+    const dataTable = document.getElementById('dataTablee')
+
+    querySnapshot.forEach(doc => {
+        shtml += `
+            <tr>
+            <td>${doc.data().HashVoto}</td>
+            <td>${doc.data().Elector}</td>
+            <td>${doc.data().Voto}</td>
+            </tr>
+        `
+    })
+
+    dataTable.innerHTML = shtml
+})
 
 export default function UrnaElectoral() {
-    return (
- 
+    
+     return (
+        
         <div className='table-responsive'>
             <table class="table table-bordered table-hover caption-top">
                 <caption>Urna electoral</caption>
@@ -16,17 +41,11 @@ export default function UrnaElectoral() {
                     <th scope="col">Hora</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>793f970c52ded1276b9264c742f19d1888cbaf73</td>
-                    <td>99800b85d3383e3a2fb45eb7d0066a4879a9dad0</td>
-                    <td>PFC</td>
-                    <td>10/05/2022</td>
-                    <td>10:00</td>
-                    </tr>
+                <tbody id="dataTablee">
                 </tbody>
             </table>
         </div>
     )
   }
+
+  
