256e1d557e75b243c9a53499ffa32b1dcdb1a038
Hanyinson Alex <calex1731@gmail.com>
2022-06-11 22:50:11 -0400

    Imprimir inicio y urna electoral

--- src/components/home.js
+++ src/components/home.js
@@ -2,14 +2,27 @@ import React, { Component } from "react";
 import Pie from "./charts/pie";
 import Bar from "./charts/bar";
 import Tabla from "./charts/tabla";
+import ReactToPrint from "react-to-print";
+
+import { Container } from "react-bootstrap";
 
 export default class home extends Component {
   render() {
     return (
       <div>
-        <Tabla />
-        <Pie />
-        <Bar />
+        <ReactToPrint
+          trigger={() => {
+            return <a href="#">Version 🖨️ Imprimir</a>;
+          }}
+          content={() => this.componentRef}
+          documentTitle="new document"
+          pageStyle="print"
+        />
+        <Container ref={(el) => (this.componentRef = el)}>
+          <Tabla />
+          <Pie />
+          <Bar />
+        </Container>
       </div>
     );
   }
