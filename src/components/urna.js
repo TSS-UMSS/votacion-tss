065b23ca70ed92d411af7cee0651d884e7c8de54
Hanyinson Alex <calex1731@gmail.com>
2022-06-14 15:43:29 -0400

    imprimir inicio y urna electoral

--- src/components/urna.js
+++ src/components/urna.js
@@ -1,17 +1,21 @@
 //import { render } from '@testing-library/react';
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
 //import { async } from '@firebase/util';
 //import { doc } from 'firebase/firestore';
 //import {getUrna} from '../confs/firebaseConf';
 import { firestore } from "../confs/firebaseConf";
-import {collection,query, where, getDocs} from "firebase/firestore";
+
+import { collection, query, where, getDocs } from "firebase/firestore";
 //import NoDisponible from './NoDisponible'
-import '../Styles/urna.css';
-import { async } from '@firebase/util';
+import "../Styles/urna.css";
+import { async } from "@firebase/util";
+import ReactToPrint from "react-to-print";
+import { Table } from "react-bootstrap";
 
 export default class urna extends Component {
   componentDidMount() {
-    insertData()
+    insertData();
   }
 
   //thevoters = consultar();
@@ -21,66 +25,75 @@ export default class urna extends Component {
         <NoDisponible mensaje="Los datos de la urna electoral aún no se encuentran disponibles."/>
       );
     }else{*/
-      return (
-        <div id="urnaa">
-          <div className='title-urna'>Urna Electoral</div>
-          <div className='table-container'>
-            <div className='table-responsive'>
-              <table className='table-urna'>
-                  <thead className='table-header'>
-                      <tr>
-                      <th>Hash del voto</th>
-                      <th>Elector</th>
-                      <th>Voto</th>
-                      <th>Fecha</th>
-                      </tr>
-                  </thead>
-                  <tbody id="dataTablee">
-                  </tbody>
-              </table>
-          </div>
-          </div>
+    return (
+      <div>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <a href="#" className="imprimir">
+                🖨️ Imprimir{" "}
+              </a>
+            );
+          }}
+          content={() => this.componentRef}
+          documentTitle="Esto es un documento"
+          pageStyle="print"
+        />
+        <div ref={(el) => (this.componentRef = el)}>
+          <div className="title-urna">Urna Electoral</div>
+          <Table striped bordered hover>
+            <thead>
+              <th>Hash del voto</th>
+              <th>Elector</th>
+              <th>Voto</th>
+              <th>Fecha y Hora</th>
+            </thead>
+            <tbody id="dataTablee"></tbody>
+          </Table>
         </div>
-      )
-    }
-    
+      </div>
+    );
   }
+}
 
-  async function insertData(thevoters) {
-    console.log('Se ejecuta insertData()')
-    //const querySnapshot = await getUrna()
-    const thequery = query(collection(firestore, "UsuarioComun"), where("VotoEstado", "==", true));
-    const voters = await getDocs(thequery);
+async function insertData(thevoters) {
+  console.log("Se ejecuta insertData()");
+  //const querySnapshot = await getUrna()
+  const thequery = query(
+    collection(firestore, "UsuarioComun"),
+    where("VotoEstado", "==", true)
+  );
+  const voters = await getDocs(thequery);
 
-    let shtml = ''
-    const dataTable = document.getElementById('dataTablee')
+  let shtml = "";
+  const dataTable = document.getElementById("dataTablee");
 
-    voters.forEach(doc => {
-        var firedate = doc.data().VotoFecha;
-        //var reactdate = firedate.toDate();
-        //var thedate = reactdate.toLocaleString();
+  voters.forEach((doc) => {
+    var firedate = doc.data().VotoFecha;
+    //var reactdate = firedate.toDate();
+    //var thedate = reactdate.toLocaleString();
 
-        var voto;
+    var voto;
 
-        if(doc.data().VotoBlanco===1){
-          voto = "Blanco";
-        }else{
-          if(doc.data().VotoNulo===1){
-            voto = "Nulo";
-          }else{
-            voto = doc.data().VotoPartidoSigla;
-          }
-        }
+    if (doc.data().VotoBlanco === 1) {
+      voto = "Blanco";
+    } else {
+      if (doc.data().VotoNulo === 1) {
+        voto = "Nulo";
+      } else {
+        voto = doc.data().VotoPartidoSigla;
+      }
+    }
 
-        shtml += `
+    shtml += `
             <tr>
             <td>${doc.data().VotoHash}</td>
             <td>${doc.data().HashSemilla}</td>
             <td>${voto}</td>
             <td>${firedate}</td>
             </tr>
-        `
-    })
+        `;
+  });
 
-    dataTable.innerHTML = shtml
-  }
+  dataTable.innerHTML = shtml;
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,84 +1,114 @@
-import React from "react";
+import React, { useRef } from "react";
 import Pie from "../components/charts/pie";
 import Bar from "../components/charts/bar";
 import Tabla from "../components/charts/tabla";
 
-import { useState,useEffect } from "react";
-import { collection, getDocs } from 'firebase/firestore'
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "../confs/firebaseConf";
+import ReactToPrint, { useReactToPrint } from "react-to-print";
+import urna from "../components/urna";
+import { Container } from "react-bootstrap";
+import jsPDF from "jspdf";
 
 const Home = () => {
-  const [users, setUsers]=useState([])
-  const usersCollectionRef = collection(firestore,"resultados")
-  useEffect(()=> {
-  //Leer     
-  const getUsers = async () =>{
-      const data = await getDocs(usersCollectionRef)
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(firestore, "resultados");
+  useEffect(() => {
+    //Leer
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
       //console.log(data)
-      setUsers(data.docs.map((doc)=>({...doc.data(),id: doc.id})))
-  }
-  getUsers()
-  },[])
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
 
-  
   // obtener datos de firestore
-  const resultados =users.map((persona)=>{
-    const info =  {id:persona.id, name:persona.name, partido:persona.partido, votos:persona.votos} 
-    return info  
-  })
-  console.log(users)
+  const resultados = users.map((persona) => {
+    const info = {
+      id: persona.id,
+      name: persona.name,
+      partido: persona.partido,
+      votos: persona.votos,
+    };
+    return info;
+  });
+  console.log(users);
 
   //obtener partidos de resultados []
-  const partidos= resultados.map((p)=>{
-    return  p.partido
-  })
+  const partidos = resultados.map((p) => {
+    return p.partido;
+  });
 
   //obtener votos de resultados []
-  const conteo = resultados.map((p)=>{
-    return p.votos
-  })
+  const conteo = resultados.map((p) => {
+    return p.votos;
+  });
   //obtener color de firestore
-  const colores  = users.map((p)=>{
-    return p.color
-  })
+  const colores = users.map((p) => {
+    return p.color;
+  });
 
-  //tabla de resultados 
-  const list = resultados 
-  
+  //tabla de resultados
+  const list = resultados;
+
   //generar un color aleatorio
-  var randomColor = "#"+Math.floor(Math.random()*16777215).toString(16);
+  var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   //console.log(randomColor)
 
-  //graficas de resultsados 
+  //graficas de resultsados
   const data = {
     labels: partidos,
     datasets: [
       {
-        label: 'Votos',
+        label: "Votos",
         data: conteo,
-        backgroundColor: colores,        
+        backgroundColor: colores,
         borderWidth: 1,
       },
-    ]
-  }
-  const esta=()=>{
-    if (users.length===0) {
-      return false
+    ],
+  };
+  const esta = () => {
+    if (users.length === 0) {
+      return false;
     } else {
-      return true
+      return true;
     }
-  }
+  };
 
-    return (
-      <div>
+  const componentRef = useRef();
+  /* const handlePrint = useReactToPrint({
+    content: () => componentRef.currente,
+  }); */
+
+  // var doc = new jsPDF();
+
+  // doc.text("Octanyan");
+
+  return (
+    <div>
+      <ReactToPrint
+        trigger={() => {
+          return (
+            <a href="#" className="imprimir">
+              🖨️ Imprimir{" "}
+            </a>
+          );
+        }}
+        content={() => componentRef.current}
+        documentTitle="Esto es un documento"
+        pageStyle="print"
+      />
+
+      <Container ref={componentRef}>
         {esta() ? (
           <>
-            <Tabla listas ={list} />
-            <Pie datos={data}/>
-            <Bar datos={data}/>
-            
+            <Tabla listas={list} />
+            <Pie datos={data} />
+            <Bar datos={data} />
           </>
-        ):(
+        ) : (
           <>
             <div className="Container">
               <h1>Bienvenido a iVote</h1>
@@ -87,9 +117,9 @@ const Home = () => {
             </div>
           </>
         )}
-        
-      </div>
-    );
-}
+      </Container>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
